ae6ef701e6d297c002b26f2565c4de55889d5fca
William <willspecker@gmail.com>
2020-11-24 17:01:55 +0100

    creating product model spec test, dropping/creating/migrating db, adding validations to product model for name and price

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '\'Products\' has db columns' do
+    it {is_expected.to have_db_column :id}
+    it {is_expected.to have_db_column :name}
+    it {is_expected.to have_db_column :price}
+  end
+
+  describe 'Validations' do
+    it {is_expected.to validate_presence_of :name}
+    it {is_expected.to validate_presence_of :price}
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(create(:product)).to be_valid
+    end
+  end
 end
