adf863de175c2a5e641d5411225cd5bab03791b2
swardbladpaulo <paulo@swardblads.se>
2020-11-28 15:08:29 +0100

    modify user_can_create_new_order_spec.rb, create and update action in controllres, divided both functionaly in testfile

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -2,6 +2,13 @@ class Api::OrdersController < ApplicationController
   def create
     order = Order.create
     order.order_items.create(product_id: params[:product_id])
-    render json: { message: 'Product was successfully added to your order!' }
+    render json: { message: 'Product was successfully added to your order!', order_id: order.id }
+  end
+
+  def update
+    order = Order.find(params[:id])
+    product = Product.find(params[:product_id])
+    order.order_items.create(product: product)
+    render json: {message: 'Product was successfully added to your order!', order_id: order.id}
   end
 end
