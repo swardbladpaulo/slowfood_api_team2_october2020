43cc3a7af3b7c3668c30eccb48da0c4ab729412a
Kyungin Na <nakyungin93@gmail.com>
2020-11-29 19:12:43 +0100

    Adding product data in data seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: 'Entrecôte with chanterelle sauce and potato gratin', description: 'Amazing starter with awesome SWEDISH chanterelle sauce', price: 12 )
+Product.create(name: 'Reindeer tartare', description: 'Minced reindeer with ponzu and gochujang, pickled daikon, soya aioli, and crispy lotus', price: 20 )
+Product.create(name: 'Bibimbap', description: 'Traditional Korean dish with marinated beef, chopped veggies and gochujang', price: 13)
+Product.create(name: 'Spicy Blue Burger', description: 'Burger with blue cheese, smoked bacon, seared jalapeno, and homemade aioli', price: 15)
+Product.create(name: 'Pannkaka', description: 'Traditional Swedish Pancake with mascerated wild lingonberry', price: 7 )
+Product.create(name: 'Bruschetta', description: 'grilled italian bread with sliced heirloom tomato, fresh basil, and burrata', price: 9 )
+Product.create(name: 'Bubble Tea', description: 'Bubbly tea from Taiwan', price: 5 )
+Product.create(name: 'Julmust', description: 'Traditional Swedish soda', price: 5)
