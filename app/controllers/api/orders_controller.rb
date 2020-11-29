4337a6d3e170ad5f7895e3e8be83c8463f8ee6b7
swardbladpaulo <72553754+swardbladpaulo@users.noreply.github.com>
2020-11-29 12:16:39 +0100

    Update orders_controller.rb

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,5 +1,5 @@
 class Api::OrdersController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   def create
     order = Order.create
     order.order_items.create(product_id: params[:product_id])
