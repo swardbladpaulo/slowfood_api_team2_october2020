34f47c6664ba28d73527ab6adf783b85541d8b8f
swardbladpaulo <paulo@swardblads.se>
2020-11-29 12:20:24 +0100

    formatting code, create data seedfile

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,4 +1,5 @@
 class Api::OrdersController < ApplicationController
+  #before_action :authenticate_user!
   def create
     order = Order.create
     order.order_items.create(product_id: params[:product_id])
@@ -9,6 +10,6 @@ class Api::OrdersController < ApplicationController
     order = Order.find(params[:id])
     product = Product.find(params[:product_id])
     order.order_items.create(product: product)
-    render json: {message: 'Product was successfully added to your order!', order_id: order.id}
+    render json: {message: 'Updated Product was successfully added to your order!', order_id: order.id}
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: 'Luke burger', description: 'food', price: 7)
+Product.create(name: 'Yoda burger', description: 'veggie', price: 3)
+Product.create(name: 'Leia burger', description: 'burger', price: 10)
